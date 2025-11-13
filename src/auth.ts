0fcb966bf46c9d2e9b0dd245f0236c6308be91d9
spiiritual <andrewnguyen2601@gmail.com>
2025-11-13 17:02:55 -0500

    enable cookie caching

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,18 @@
-import { betterAuth } from "better-auth";
-import { Pool } from "pg";
+import {betterAuth} from 'better-auth';
+import {pool} from './db';
 
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING
-  }),
+  database: pool,
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }
-  }
+    },
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 10,
+    },
+  },
 });
